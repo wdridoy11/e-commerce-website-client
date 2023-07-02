c2f2818838cfc910168b67a2a48a1f93bb3a4914
Ridoy Sharif <developersridoy@gmail.com>
2023-07-02 08:42:12 +0600

    remove all content

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ import router from "./routes/routes/routes";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    
   </React.StrictMode>
 );
