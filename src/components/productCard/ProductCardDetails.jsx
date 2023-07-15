8b95f848689ae2f0b56dd7f24703f7a190b557da
Ridoy Sharif <developersridoy@gmail.com>
2023-07-15 13:04:52 +0600

    Related Product card added and modifi product details page

--- src/components/productCard/ProductCardDetails.jsx
+++ src/components/productCard/ProductCardDetails.jsx
@@ -3,6 +3,7 @@ import '@smastrom/react-rating/style.css'
 import { Rating } from '@smastrom/react-rating'
 import { FaMinus } from 'react-icons/fa';
 import { MdAdd } from 'react-icons/md';
+import RelatedProduct from './RelatedProduct';
 const img =`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsKx_YNcf4Fi_7Tc9Sj-19ZWnxJV6xfte9KQLMn3zZ2G4ffXeNS38-omkB7yw-E4JaBRQ&usqp=CAU`
 const ProductCardDetails = () => {
     // react reating
@@ -17,56 +18,68 @@ const ProductCardDetails = () => {
   return (
     <div className='mt-10 py-20'>
         <div className='container mx-auto px-5'>
-            <div className='grid grid-cols-7 bg-white gap-5 p-10'>
-                <div className='col-span-2'>
-                    <div className='w-full h-96 overflow-hidden'>
-                        <img className='w-full mx-auto' src={img} alt="" />
+            {/* product details page gird start*/}
+            <div className='grid grid-cols-5 gap-5'>
+                {/* product details start */}
+                <div className='col-span-4 bg-white p-10'>
+                    <div className='grid grid-cols-3 gap-5'>
+                        <div className='col-span-1'>
+                            <div className='w-full h-96 overflow-hidden'>
+                                <img className='w-full mx-auto' src={img} alt="" />
+                            </div>
+                        </div>
+                        <div className='col-span-2'>
+                            <h3 className='text-2xl font-semibold text-black mb-3'>New Trendy Look</h3>
+                            <div className='flex gap-1 items-center'>
+                                <div>
+                                    <Rating style={{ maxWidth: 60 }} value={rating} onChange={setRating} />
+                                </div>
+                                <div className='text-base font-semibold'>
+                                    (10)
+                                </div>
+                            </div>
+                            <div className='mt-3'>
+                                <p className='text-base text-[#9e9e9e] font-medium'>Brand: <span className='text-black'>Apple</span> </p>
+                                <p className='text-base text-[#9e9e9e] font-medium'>Category: <span className='text-black'>Apple</span></p>
+                            </div>
+                            <div className="divider"></div> 
+                            <p className='text-4xl font-medium text-[#f85606] mb-5'>$ <span>100</span></p>
+                            {/* quentity area start */}
+                            <div className='flex items-center gap-5 mb-5'>
+                                <p className='text-base text-black font-medium'>Quantity</p>
+                                <div className='flex items-center gap-2'>
+                                    <button className='bg-[#eff0f5] p-2 rounded-sm'>
+                                        <FaMinus className='text-lg text-black' />
+                                    </button>
+                                    <input 
+                                        type="number" 
+                                        defaultValue={`1`} 
+                                        name="quantity" 
+                                        id="quantity" 
+                                        min="1" 
+                                        className='p-2' 
+                                    />
+                                    <button className='bg-[#eff0f5] p-2 rounded-md'>
+                                        <MdAdd className='text-lg text-black' />
+                                    </button>
+                                </div>
+                            </div>
+                            {/* quentity area end */}
+                            <div className='w-1/2 grid md:grid-cols-2 gap-2'>
+                                <button className='w-full py-2 bg-blue-500 text-white font-medium rounded-sm hover:bg-black duration-500'>Buy Now</button>
+                                <button className='w-full py-2 bg-[#FF5039] text-white font-medium rounded-sm hover:bg-black duration-500'>Add To Card</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className='col-span-4'>
-                    <h3 className='text-2xl font-semibold text-black mb-3'>New Trendy Look</h3>
-                    <div className='flex gap-1 items-center'>
-                        <div>
-                            <Rating style={{ maxWidth: 60 }} value={rating} onChange={setRating} />
-                        </div>
-                        <div className='text-base font-semibold'>
-                            (10)
-                        </div>
-                    </div>
-                    <div className='mt-3'>
-                        <p className='text-base text-[#9e9e9e] font-medium'>Brand: <span className='text-black'>Apple</span> </p>
-                        <p className='text-base text-[#9e9e9e] font-medium'>Category: <span className='text-black'>Apple</span></p>
-                    </div>
-                    <div className="divider"></div> 
-                    <p className='text-4xl font-medium text-[#f85606] mb-5'>$100</p>
-                    <div className='flex items-center gap-5'>
-                        <p>Quantity</p>
-                        <div className='flex items-center gap-2'>
-                            <button className='bg-[#eff0f5] p-2 rounded-md'>
-                                <FaMinus className='text-lg text-black' />
-                            </button>
-                            <input 
-                                type="number" 
-                                defaultValue={`1`} 
-                                name="quantity" 
-                                id="quantity" 
-                                min="1" 
-                                className='p-2' 
-                            />
-                            <button className='bg-[#eff0f5] p-2 rounded-md'>
-                                <MdAdd className='text-lg text-black' />
-                            </button>
-                        </div>
-                    </div>
-                    <div>
-                        <button className='px-5 py-2 bg-blue-500 text-white font-medium'>Buy Now</button>
-                        <button>Add To Card</button>
-                    </div>
+                {/* product details end */}
+                {/* Related product start */}
+                <div className='bg-white p-2'>
+                     <RelatedProduct></RelatedProduct>
                 </div>
-                <div>
-                    <img className='w-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF_KI8okFTxbQmmE6W4yn3szv0qGsT3vftHNozHfxc&s" alt="" />
-                </div>
+                {/* Related product end */}
             </div>
+            {/* product details page gird end*/}
         </div>
     </div>
   )
---
+++ src/components/productCard/RelatedProduct.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import '@smastrom/react-rating/style.css'
+import { Rating } from '@smastrom/react-rating'
+const image =`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsKx_YNcf4Fi_7Tc9Sj-19ZWnxJV6xfte9KQLMn3zZ2G4ffXeNS38-omkB7yw-E4JaBRQ&usqp=CAU`
+const phone_name=`Apple`
+const RelatedProduct = () => {
+    // react reating
+    const [rating, setRating] = useState(4)
+    // product data destructuring
+    // const {image, phone_name } = product;
+  return (
+    <Link to={`productDetails`} className='hover:shadow-md duration-150'>
+        <div className='bg-white rounded-sm'>
+            <div className='w-full h-48 overflow-hidden'>
+                <img className='w-full mx-auto' src={image} alt={phone_name} />
+            </div>
+            <div className='p-2'>
+                <h4 className='text-sm text-[#212121] font-medium'>{phone_name.length>20? <>{phone_name.slice(0,20)}...</>:phone_name}</h4>
+                <p className='text-lg font-medium text-[#f85606]'>$100</p>
+                <div className='flex gap-2 items-center'>
+                    <div>
+                        <Rating style={{ maxWidth: 60 }} value={rating} onChange={setRating} />
+                    </div>
+                    <div className='text-base font-semibold'>
+                        (10)
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Link>
+  )
+}
+
+export default RelatedProduct
