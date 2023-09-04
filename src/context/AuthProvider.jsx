9b67c16637ba6cba0f2767aa521fe8a417bb34a4
Ridoy Sharif <developersridoy@gmail.com>
2023-09-04 12:29:46 +0600

    login system added

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,33 +1,73 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 import {
   getAuth,
   signInWithPopup,
   GoogleAuthProvider,
   GithubAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
   } from 'firebase/auth'
 import app from '../utils/firebase/firebase.config';
 export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
+
   const auth = getAuth(app);
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true)
+
+   // create user using email and pasword
+   const createUserUsingEmail=(email, passages)=>{
+    setLoading(true)
+    return createUserWithEmailAndPassword(auth,email,passages)
+   }
 
   // create accout using google
   const createUserUsingGoogle=()=>{
+    setLoading(true)
     const googleProvider = new GoogleAuthProvider();
     return signInWithPopup(auth,googleProvider)
   }
 
   // create accout using Github
   const createUserUsingGithub=()=>{
+    setLoading(true)
     const githubProvider = new GithubAuthProvider();
     return signInWithPopup(auth,githubProvider)
   }
 
+  // user login
+  const userLogin=(email,passages)=>{
+    setLoading(true)
+    return signInWithEmailAndPassword(auth,email,passages)
+  }
   
+  // user logout
+  const logOut=()=>{
+    setLoading(true)
+    return signOut(auth)
+  } 
+
+  useEffect(()=>{
+    const unSubscribe = onAuthStateChanged(auth,currentUser=>{
+      setUser(currentUser)
+      setLoading(true)
+    })
+    return ()=>{
+      return unSubscribe();
+    }
+  },[])
+
   const userInfo ={
+    user,
+    logOut,
+    loading,
+    userLogin,
+    createUserUsingEmail,
     createUserUsingGoogle,
-    createUserUsingGithub
+    createUserUsingGithub,
   }
 
   return (
