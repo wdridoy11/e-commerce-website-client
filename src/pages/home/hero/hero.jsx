f11c29923dcc7873fc980666d514a00d432e8372
Ridoy Sharif <developersridoy@gmail.com>
2023-10-09 14:14:25 +0600

    hero section slider added and change some image

--- src/pages/home/hero/hero.jsx
+++ src/pages/home/hero/hero.jsx
@@ -1,24 +1,57 @@
 import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import hero1 from '../../../assets/hero-04.png'
-import hero2 from '../../../assets/hero-03.jpg'
-import hero3 from '../../../assets/offer-01.jpg'
+import 'swiper/css';
+import "./slider.css"
+import 'swiper/css/pagination';
 import { Link } from 'react-router-dom'
-const img = `https://www.cloud.ryanscomputers.com/cdn/sliders/META-Quest-2-128GB-All-in-One-VR-System-Banner_1688808664.webp`
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import hero1 from '../../../assets/hero-1.png'
+import hero2 from '../../../assets/hero-2.png'
+import hero3 from '../../../assets/hero-3.png'
+import hero4 from '../../../assets/hero-4.jpg'
+import hero5 from '../../../assets/hero-5.jpg'
+import hero6 from '../../../assets/hero-6.png'
+// Import Swiper React components
+
+const heroSlider=[
+    {
+        img:hero1,
+        title:"iPhone",
+        description:"Discount up to 5%",
+        link:"google.com",
+        color:"black"
+    },
+    {
+        img:hero2,
+    },
+    {
+        img:hero3,
+        title:"Mackbook",
+        description:"Discount up to 10%",
+        link:"google.com",
+        color:"black"
+    }
+]
 
 const Hero = () => {
   return (
     <div className='bg-[#F6F6F6] py-10'>
         <div className='container mx-auto'>
             <div className='grid grid-cols-2 gap-5'>
-                <div className='relative'>
-                    <div className='absolute top-1/3 left-10'>
-                        <h3 className='text-2xl font-medium text-black'>iPhone</h3>
-                        <p className='text-base text-black font-normal mt-2 mb-3'>Discount up to 5%</p>
-                        <Link className='bg-white text-black font-medium px-5 py-2 rounded-md 
-                        inline-block hover:bg-black hover:text-white duration-500'>Buy Now</Link>
-                    </div>
-                    <img className='rounded-md h-[520px] w-full object-cover' src={hero1} alt="" />
+                <div>
+                    <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+                        {heroSlider.map((item,index)=><SwiperSlide key={index}>
+                            <div className='relative'>
+                                <img className='w-full h-[520px] object-cover rounded-md' src={item.img} alt="" /> 
+                                <div className='absolute top-1/3 left-10 text-left'>
+                                    {item.title && <h3 className={`text-2xl font-semibold ${item.color === "white" ? "text-white" : "text-black"}`}>{item.title}</h3>}
+                                    {item.description && <p className={`text-base text-black font-normal mt-2 mb-3 ${item.color === "white"?"text-white" :"text-black"}`}>{item.description}</p>}
+                                    {item.link && <Link to={item.link} className='bg-white text-black font-medium px-5 py-2 rounded-md 
+                                    inline-block hover:bg-black hover:text-white duration-500'>Buy Now</Link>}
+                                </div>
+                            </div>
+                        </SwiperSlide>)}
+                    </Swiper>
                 </div>
                 <div className='flex flex-col gap-5'>
                     <div className='relative'>
@@ -26,7 +59,7 @@ const Hero = () => {
                             <h3 className='text-3xl font-semibold text-white mb-4'>Aurora Headset</h3>
                             <Link className='bg-blue-500 px-10 py-3 rounded-md text-white hover:bg-black duration-500'>Buy Now</Link>
                         </div>
-                        <img className='rounded-md' src={hero2} alt="" />
+                        <img className='rounded-md' src={hero4} alt="" />
                     </div>
                     <div className='grid grid-cols-2 gap-5'>
                         <div className='relative'>
@@ -36,16 +69,16 @@ const Hero = () => {
                                 <Link className='bg-white text-black font-medium px-5 py-2 rounded-md 
                                 inline-block hover:bg-black hover:text-white duration-500'>View Details</Link>
                             </div>
-                            <img className='rounded-md w-full h-48 object-cover' src={hero3} alt="" />
+                            <img className='rounded-md w-full h-48 object-cover' src={hero5} alt="" />
                         </div>
                         <div className='relative'>
                             <div className='absolute top-1/4 left-10'>
-                                <h3 className='text-lg font-medium text-white'>Xiaomi MI 11</h3>
-                                <p className='text-base text-white font-normal mt-2 mb-3'>Discount up to 30%</p>
+                                <h3 className='text-lg font-medium text-black'>Xiaomi MI 11</h3>
+                                <p className='text-base text-black font-normal mt-2 mb-3'>Discount up to 30%</p>
                                 <Link className='bg-white text-black font-medium px-5 py-2 rounded-md 
                                 inline-block hover:bg-black hover:text-white duration-500'>View Details</Link>
                             </div>
-                            <img className='rounded-md w-full h-48 object-cover' src={hero3} alt="" />
+                            <img className='rounded-md w-full h-48 object-cover' src={hero6} alt="" />
                         </div>
                     </div>
                 </div>
