1aee64e3d582757d3548c70019ab11be5bb22634
Ridoy Sharif <developersridoy@gmail.com>
2023-10-09 16:08:49 +0600

    categories data added

--- src/components/categories/CategoriesData.js
+++ src/components/categories/CategoriesData.js
@@ -1,7 +1,10 @@
-import tv from '../../assets/categories/tv.png';
+import ipad from '../../assets/categories/ipad.png';
+import imac from '../../assets/categories/imac.png';
+import mobile from '../../assets/categories/mobile.png';
 import iphone from '../../assets/categories/iphone.png';
 import mackbook from '../../assets/categories/mackbook.png';
 import headphone from '../../assets/categories/headphone.png';
+import televisions from '../../assets/categories/televisions.png'; 
 
 export const categoriesData =[
     {
@@ -11,16 +14,31 @@ export const categoriesData =[
     },
     {
         id:2,
+        title:"Imac",
+        icon:imac
+    },
+    {
+        id:3,
         title:"iphone",
         icon:iphone
     },
     {
-        id:3,
-        title:"TV",
-        icon:tv
+        id:4,
+        title:"Ipad",
+        icon:ipad
     },
     {
-        id:4,
+        id:5,
+        title:"Mobile",
+        icon:mobile
+    },
+    {
+        id:6,
+        title:"Televisions",
+        icon:televisions
+    },
+    {
+        id:7,
         title:"Headphone",
         icon:headphone
     },
