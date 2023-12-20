7268dbc2a22cb722fe505e2cee0846abcfe8aa21
Ridoy Sharif <developersridoy@gmail.com>
2023-12-20 17:57:41 +0600

    order info display successful

--- src/pages/order/Order.jsx
+++ src/pages/order/Order.jsx
@@ -6,18 +6,19 @@ const Order = () => {
     useEffect(()=>{
         fetch(`${process.env.REACT_APP_API_URL}/order`)
         .then((res)=>res.json())
-        .then((data)=>setOrderInfo(data))
+        .then((data)=>{
+            data?.map((orderDetails)=>{
+                setOrderInfo(orderDetails.card)
+            })
+        })
     },[])
+    
   return (
     <div>
         <div className='w-full h-screen pt-10 lg:px-10'>
             <div className='bg-white p-5'>
                 <div>
-                    <h1 className='text-lg font-medium mb-2'>Your Order ID: <span>17135021605139</span> (2 items)</h1>
-                    <h1 className='text-lg font-medium mb-5'>Payable Amount: TK. <span>713</span></h1>
-                </div>
-                <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-5'>
-                    <OrderCard />
+                    <OrderCard card={orderInfo} />
                 </div>
             </div>
         </div>
--- src/pages/order/OrderCard.jsx
+++ src/pages/order/OrderCard.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const OrderCard = () => {
+const OrderCard = ({card}) => {
+
   return (
-    <div className='bg-white rounded-md p-5 border rounded-b'>
-        <Link to={""}>
-            <div>
-                <div className='w-full h-48 overflow-hidden'>
-                    <img className='w-9/12 mx-auto pb-5 hover:scale-110 duration-500' src="https://adminapi.applegadgetsbd.com/storage/media/large/2324-41148.jpg" />
+    <div className='bg-white rounded-md '>
+        <div>
+            <h1 className='text-lg font-medium mb-2'>Your Order ID: <span>17135021605139</span> ({card?.length} items)</h1>
+            <h1 className='text-lg font-medium mb-5'>Payable Amount: TK. <span>713</span></h1>
+        </div>
+        <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-5'>
+            {card?.map((product)=><Link to={`/productDetails/${product._id}`} className='border p-5 rounded-b'>
+                <div>
+                    <div className='w-full h-48 overflow-hidden'>
+                        <img className='w-9/12 mx-auto pb-5 hover:scale-110 duration-500' src={product.product_image} />
+                    </div>
                 </div>
-            </div>
-            <div className='mt-3'>
-                <h4 className='text-base text-[#212121] font-medium'>MacBook Pro M1 8/256</h4>
-                <h4 className='text-xl font-semibold text-blue-500 mt-2'>$100</h4>
-            </div>
-        </Link>
+                <div className='mt-3'>
+                    <h4 className='text-base text-[#212121] font-medium'>
+                        {product.product_name.length>30? <>{product.product_name.slice(0,30)}...</>:product.product_name}
+                    </h4>
+                    <h4 className='text-xl font-semibold text-blue-500 mt-2'>{product.price}</h4>
+                </div>
+            </Link>)}
+        </div>
     </div>
   )
 }
